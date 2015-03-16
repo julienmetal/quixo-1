eeb3966ab85678183b68e63f4cec84825b966801
verdernel <verdernel@debb.home>
2015-03-16 15:22:45 +0100

    add images

--- quixo.cpp
+++ quixo.cpp
@@ -1,11 +1,22 @@
 #include "quixo.h"
 #include "ui_quixo.h"
+#include <QGraphicsScene>
+#include <QGraphicsPixmapItem>
+#include <QPixmap>
 
 Quixo::Quixo(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Quixo)
 {
     ui->setupUi(this);
+    QGraphicsScene * ma_scene;
+    ma_scene = new QGraphicsScene();
+    QGraphicsPixmapItem * mon_item;
+    mon_item = new QGraphicsPixmapItem();
+    QPixmap * mon_image = new QPixmap(":/1.png");
+    mon_item->setPixmap(* mon_image);
+    ma_scene->addItem(mon_item);
+    ui->ma_vue->setScene(ma_scene);
 }
 
 Quixo::~Quixo()
